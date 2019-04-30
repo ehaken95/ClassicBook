c7ce07b92bfdf031c5424ece8e5c3ddde89c6bf1
SeoSungJoon <seojj95@naver.com>
2019-04-30 23:52:22 +0900

    [추가,수정]
    -폰트추가(나눔바른고딕)
    -버튼클릭이펙트xml추가
    -색상코드 정형화
    -string추가중
    -로그인화면 수정중

--- app/src/main/java/com/seosj/classicbook/LoginActivity.java
+++ app/src/main/java/com/seosj/classicbook/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.seosj.classicbook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+
+        EditText text_id = findViewById(R.id.textID);
+        EditText text_pw = findViewById(R.id.textPW);
+        Button button_login=findViewById(R.id.buttonLogin);
+        ScrollView scrollView = findViewById(R.id.sv_root);
+        /*
+        editText.getText.toString();을 하면 String객체로 Text를 리턴하게 된다.
+        또한 이 String이 공백인지 아닌지를 체크하기 위해서는 다음과 같이 처리를 해줘도 된다.
+        if ( editText.getText.toString().length() == 0 ) {
+            //공백일 때 처리할 내용
+
+        } else {
+            //공백이 아닐 때 처리할 내용
+        }
+        으로 공백체크도 해줄 수 있다.
+        */
+
+
+
+
+
+
+
     }
 }

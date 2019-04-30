d0c7a5f6a1fb022923a7c611dfcb734dd6088e9f
SeoSungJoon <seojj95@naver.com>
2019-05-01 00:55:32 +0900

    [추가,수정]
    -메인에서 뒤로가기시 종료되는 기능 추가
    -라디오버튼->체크박스
    -버튼 모서리 둥글게

--- app/src/main/java/com/seosj/classicbook/LoginActivity.java
+++ app/src/main/java/com/seosj/classicbook/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.seosj.classicbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ScrollView;
 
@@ -14,11 +16,15 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
+        //자동로그인일 경우 메인으로 바로 가야한다!
+
+
 
         EditText text_id = findViewById(R.id.textID);
         EditText text_pw = findViewById(R.id.textPW);
         Button button_login=findViewById(R.id.buttonLogin);
         ScrollView scrollView = findViewById(R.id.sv_root);
+        CheckBox autoLogin = findViewById(R.id.autologin);
         /*
         editText.getText.toString();을 하면 String객체로 Text를 리턴하게 된다.
         또한 이 String이 공백인지 아닌지를 체크하기 위해서는 다음과 같이 처리를 해줘도 된다.
@@ -32,10 +38,27 @@ public class LoginActivity extends AppCompatActivity {
         */
 
 
+        //자동로그인 선택 상태 체크
+        if(autoLogin.isChecked()){
+            //TODO
+        }else{
+            //TODO
+        }
+
+
+        button_login.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+            startActivity(intent);
+        });
+
 
 
 
 
 
     }
+
+
+
+
 }
--- app/src/main/java/com/seosj/classicbook/MainActivity.java
+++ app/src/main/java/com/seosj/classicbook/MainActivity.java
@@ -1,7 +1,8 @@
 package com.seosj.classicbook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,10 +10,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        int test;
-        String a;
-        String a2;
+    //자동로그인일 경우 메인으로 바로 와야한다!
+    //Bottom Navigation Activity로 바꿀것
 
     }
+
+
+
+
+
+
+
+
+
+    //뒤로가기 버튼을 두번 연속으로 눌러야 종료되게끔 하는 메소드
+    private long time= 0;
+    @Override
+    public void onBackPressed(){
+        if(System.currentTimeMillis()-time>=2000){
+            time=System.currentTimeMillis();
+            Toast.makeText(getApplicationContext(),"뒤로 버튼을 한번 더 누르면 종료합니다.",Toast.LENGTH_SHORT).show();
+        }else if(System.currentTimeMillis()-time<2000){
+            finish();
+        }
+    }
+
+
+
+
 }

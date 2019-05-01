5a4b47d32acb36f954e6727fcab085ac9485776b
SeoSungJoon <seojj95@naver.com>
2019-05-02 03:04:10 +0900

    [추가,수정,오류]
    ##recyclerview 오류중. 해결중
    -각 도서 카테고리버튼 CustomView추가
    -CustomView UI제작
    -각종 UI제작중
    -string->@string화
    -recyclerview제작중

--- app/src/main/java/com/seosj/classicbook/MainActivity.java
+++ app/src/main/java/com/seosj/classicbook/MainActivity.java
@@ -1,13 +1,16 @@
 package com.seosj.classicbook;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,44 +21,43 @@ public class MainActivity extends AppCompatActivity {
     private Menu3Fragment menu3Fragment = new Menu3Fragment();
     private Menu4Fragment menu4Fragment = new Menu4Fragment();
 
+    private RecyclerAdapter adapter;
 
 
-
-
-
-
+    //인증현황 menu_home
+    //예약신청 menu_reservation_test
+    //도서 검색 menu_search
+    //설정 menu_settings
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        //인증현황 menu_home
-        //예약신청 menu_reservation_test
-        //도서 검색 menu_search
-        //설정 menu_settings
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            switch (item.getItemId()) {
-                case R.id.navigation_1:
-                    transaction.replace(R.id.frame_layout,menu1Fragment).commitAllowingStateLoss();
-                    return true;
-                case R.id.navigation_2:
-                    transaction.replace(R.id.frame_layout,menu2Fragment).commitAllowingStateLoss();
-                    return true;
-                case R.id.navigation_3:
-                    transaction.replace(R.id.frame_layout,menu3Fragment).commitAllowingStateLoss();
-                    return true;
-                case R.id.navigation_4:
-                    transaction.replace(R.id.frame_layout,menu4Fragment).commitAllowingStateLoss();
-                    return true;
-            }
-            return false;
-        }
-    };
+            = item -> {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch (item.getItemId()) {
+                    case R.id.navigation_1:
+                        transaction.replace(R.id.frame_layout,menu1Fragment).commitAllowingStateLoss();
+                        return true;
+                    case R.id.navigation_2:
+                        transaction.replace(R.id.frame_layout,menu2Fragment).commitAllowingStateLoss();
+                        return true;
+                    case R.id.navigation_3:
+                        transaction.replace(R.id.frame_layout,menu3Fragment).commitAllowingStateLoss();
+                        return true;
+                    case R.id.navigation_4:
+                        transaction.replace(R.id.frame_layout,menu4Fragment).commitAllowingStateLoss();
+                        return true;
+                }
+                return false;
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        init();
+
+        getData();
+
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
         // BottomNavigationView 메뉴를 선택할 때마다 위치가 변하지 않도록
@@ -70,8 +72,32 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    private void init(){
 
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView1);
+        recyclerView.setLayoutManager(linearLayoutManager);
 
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private void getData(){
+
+        List<String> listTitle = Arrays.asList("2019-03-19\n10:00~10:20");
+        List<String> listContent = Arrays.asList("광108B\n도서명: 실락원");
+        List<Integer> listResId = Arrays.asList(R.drawable.ic_button_clickarrow);
+
+        Data data = new Data();
+        data.setTitle(listTitle.get(0));
+        data.setContent(listContent.get(0));
+        data.setResId(listResId.get(0));
+        adapter.addItem(data);
+
+        adapter.notifyDataSetChanged();
+
+    }
 
 
     //뒤로가기 버튼을 두번 연속으로 눌러야 종료되게끔 하는 메소드
